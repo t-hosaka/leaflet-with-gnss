7a34458a5fe2ebdc76c8513d24133b8dd4c51f75
Zoar <zoar@k-side.net>
2015-02-03 17:07:10 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+var map = L.map('map',{maxZoom: 23});
+
+var attr = "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院 地理院地図</a>";
+
+L.tileLayer('http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+   attribution: attr
+}).addTo(map);
+
+map.setView([36.06, 139.3440306], 10);
+
+//ポリゴンの基本的な色を設定する
+function style(feature) {
+   return {
+      fillColor: '#2A4EFC',
+      weight: 1,
+      opacity: 1,
+      color: 'white',
+      fillOpacity: 0.5
+   }
+}
+
+//マウスオーバー、マウスアウト、クリックのイベントを登録する
+function onEachFeature(feature, layer) {
+      layer.bindPopup('<p>Hey!</p>')
+}
+
+var geojson;
+geojson = L.geoJson(area, {
+   style: style,
+   onEachFeature: onEachFeature
+}).addTo(map);
