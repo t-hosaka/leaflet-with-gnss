13f9cebacd2fbcd301b42179c5e7a1ee3bf61d17
Zoar <zoar@k-side.net>
2015-02-04 09:14:53 +0900

    コメントの追加

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 
-var map = L.map('map',{maxZoom: 23});
+var map = L.map('map');
 
 var attr = "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院 地理院地図</a>";
+var data = "<a href='http://nlftp.mlit.go.jp/ksj/' target='_blank'>国土数値情報</a>"
 
 L.tileLayer('http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
-   attribution: attr
+   attribution: attr + " | " + data
 }).addTo(map);
 
 map.setView([36.06, 139.3440306], 10);
@@ -26,7 +27,16 @@ function onEachFeature(feature, layer) {
    layer.bindPopup('<b>' + props.施設名称 + '</b><br />' + props.住所)
 }
 
+//GeoJsonの読み込み
 var geojson;
 geojson = L.geoJson(points, {
    onEachFeature: onEachFeature
 }).addTo(map);
+
+//GNSS制御
+var gnss = L.control({ position: 'bottomright' });
+gnss.onAdd = function (map) {
+   var div = L.DomUtil.create('div', 'gnsspanel');
+   div.innerHTML = '';
+   return div;
+};
